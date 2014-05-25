7a61dde95090d157ebf7afd63d8a3d8f3a0c1c48
origami <origami@Macintosh-1381.local>
2014-05-25 17:15:57 -0400

    added core iOS app

---
+++ WordListsParser/WordListsParser/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WordListsParser
+//
+//  Created by origami on 5/25/14.
+//  Copyright (c) 2014 Nebulous Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
